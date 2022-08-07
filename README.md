1cd3d201d8ada87b16c824d89c5f891776685793
Bruno S. Machado <bruno.sabbagmachado@outlook.com>
2022-08-07 16:24:06 -0300

    curso e projeto finalizados

---
+++ README.md
@@ -0,0 +1,7 @@
+# JavaScript para Web: Crie páginas dinâmicas
+
+Projeto contendo os códigos que fiz durante o curso [JavaScript para Web: Crie páginas dinâmicas](https://cursos.alura.com.br/course/javascript-web-paginas-dinamicas) da Alura. 
+
+Nesse curso aprendi e revisei conceitos de JavaScript com foco em arrays e conseguir usar objetos e elementos criados no HTML.
+
+Para testar o projeto clique [aqui](https://brunosabbagmachado.github.io/alura-midi/).
--- main.js
+++ main.js
@@ -1,5 +1,32 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        console.log('Elemento não encontrado ou seletor inválido')
+    }
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (var i = 0; i < listaDeTeclas.length; i++) {
+
+    const tecla = listaDeTeclas[i];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+}
